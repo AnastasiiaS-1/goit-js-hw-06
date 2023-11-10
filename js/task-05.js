84d38d3a3beaac55184e133a8ab5b4199330fda7
AnastasiiaS-1 <stacey.vlas56@gmail.com>
2023-11-10 15:21:21 +0200

    chaange

--- js/task-05.js
+++ js/task-05.js
@@ -1,10 +1,11 @@
-const inputNameEl = document.querySelector("name-input");
-const spanNameEl = document.querySelector("name-output");
+const inputNameEl = document.getElementById("name-input");
+const spanNameEl = document.getElementById("name-output");
 
-inputNameEl.addEventListener("input", function() {
-const inputText = inputNameEl.value;
+inputNameEl.addEventListener("input", function () {
+  const inputText = inputNameEl.value;
   if (inputText.trim() === "") {
-  spanNameEl.textContent = "Anonymous";
+    spanNameEl.textContent = "Anonymous";
   } else {
-  spanNameEl.textContent = inputText;
-
+    spanNameEl.textContent = inputText;
+  }
+});
